6e21d23fe06b9d0f2d9a5a954a7e0321120d4112
dhela19 <ubongabasijerome8@gmail.com>
2023-03-06 21:25:42 +0100

    Contact Pages Finished

---
+++ contact.js
@@ -0,0 +1,17 @@
+//declarations of global variables
+console.log("contact Script loaded")
+contactContainer = document.getElementsByClassName("contactContainer");
+firstContact = document.getElementById("firstContact");
+
+function contactContainerAnimationStart(){
+    console.log("contact Animation Started")
+}
+redirectAmazon=()=>{
+    window.location.href="https://www.amazon.com/amazon-fashion/b?ie=UTF8&node=7141123011"
+}
+redirectEbay=()=>{
+    window.location.href="https://ebay.to/3mpy0d3"
+}
+redirectPanda=()=>{
+    window.location.href = "#"
+}
--- general.js
+++ general.js
@@ -2,13 +2,10 @@ console.log("general script loaded")
 
 //Declarations of Variables
 productSearchBar = document.getElementById('productSearchBar')
+footerIcons = document.querySelectorAll('.partnersEdit')
 
 
 
-
-productSearchBar.addEventListener(onfocus, ()=>{
-    
-})
 function animateSearchBar(){
     productSearchBar.style = "  transition:1000ms; border-radius:0rem; width:40%;"
     console.log("searchBar.animate")
@@ -18,3 +15,13 @@ function releaseAnimation(){
     productSearchBar.style = "  transition:1000ms; border-radius:1rem; width:auto%;"
     console.log("productSearchBar.return")
 }
+
+function home(){
+    window.location.href="/"
+}
+// toContact=()=>{
+    
+// }
+footerIcons.addEventListener("click", ()=>{
+    window.location.href="./contact.html"
+})
